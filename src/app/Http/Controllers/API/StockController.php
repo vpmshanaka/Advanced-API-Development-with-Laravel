6784fa2a58dee853f98d6979baa1c999b231c8f7
Mahesh Shanaka <shanakavpm@gmail.com>
2023-10-01 23:47:51 +0530

    Updated test cases

--- src/app/Http/Controllers/API/StockController.php
+++ src/app/Http/Controllers/API/StockController.php
@@ -72,52 +72,52 @@ class StockController extends BaseController
         return $this->sendResponse(new StockResource($stock), 'Stock retrieved successfully.');
     }
 
-    // /**
-    //  * Update the specified resource in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @param  Stock $stock
-    //  * @return \Illuminate\Http\JsonResponse
-    //  */
-    // public function update(Request $request, Stock $stock): JsonResponse
-    // {
-    //     try {
-    //         $input = $request->all();
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Stock $stock
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, Stock $stock): JsonResponse
+    {
+        try {
+            $input = $request->all();
             
-    //         $validator = Validator::make($input, [
-    //             'product_id' => 'integer|required|exists:products,id',
-    //             'quantity' => 'integer|nullable',
-    //         ]);
+            $validator = Validator::make($input, [
+                'product_id' => 'integer|required|exists:products,id',
+                'quantity' => 'integer|nullable',
+            ]);
             
-    //         if ($validator->fails()) {
-    //             return $this->sendError('Validation Error.', $validator->errors());
-    //         }
+            if ($validator->fails()) {
+                return $this->sendError('Validation Error.', $validator->errors());
+            }
             
-    //         // $stock->name = $input['name'];
-    //         // $stock->detail = $input['detail'];
-    //         $stock->save($input);
+            // $stock->name = $input['name'];
+            // $stock->detail = $input['detail'];
+            $stock->save($input);
             
-    //         return $this->sendResponse(new StockResource($stock), 'Stock updated successfully.');
-    //     } catch (\Throwable $e) {
-    //         return $this->sendError('Error', $e->getMessage());
-    //     }
+            return $this->sendResponse(new StockResource($stock), 'Stock updated successfully.');
+        } catch (\Throwable $e) {
+            return $this->sendError('Error', $e->getMessage());
+        }
         
-    // }
+    }
 
-    // /**
-    //  * Remove the specified resource from storage.
-    //  *
-    //  * @param  Stock $stock
-    //  * @return \Illuminate\Http\JsonResponse
-    //  */
-    // public function destroy(Stock $stock): JsonResponse
-    // {
-    //     try {
-    //         $stock->delete();
-    //         return $this->sendResponse([], 'Stock deleted successfully.');
-    //     } catch (\Throwable $e) {
-    //         return $this->sendError('Error', $e->getMessage());
-    //     }
-    // }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Stock $stock
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Stock $stock): JsonResponse
+    {
+        try {
+            $stock->delete();
+            return $this->sendResponse([], 'Stock deleted successfully.');
+        } catch (\Throwable $e) {
+            return $this->sendError('Error', $e->getMessage());
+        }
+    }
     
 }
